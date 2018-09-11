6bce597d0cf01cbaad24af68221af6a7ca4e902a
Telera Nicolas <nicolas_telera@dm-c07vt0xqg1j1.groupevsc.com>
2018-09-11 13:35:27 +0200

    faces now blurred  / unblurred on click

--- app/src/main/java/com/nicolastelera/anonymize/BlurViewGroup.kt
+++ app/src/main/java/com/nicolastelera/anonymize/BlurViewGroup.kt
@@ -14,7 +14,13 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.face.FirebaseVisionFace
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
 
-private data class FaceRectangle(val bounds: Rect, val rotationX: Float, val rotationY: Float)
+private data class FaceRectangle(
+        val bounds: Rect,
+        val rotationX: Float,
+        val rotationY: Float,
+        var isApplied: Boolean = false,
+        var shouldBeBlurred: Boolean = false
+)
 
 class BlurViewGroup(context: Context, attrsSet: AttributeSet) : RelativeLayout(context, attrsSet) {
 
@@ -24,13 +30,13 @@ class BlurViewGroup(context: Context, attrsSet: AttributeSet) : RelativeLayout(c
     private var scaleFactor: Float = 0f
 
     private val detector = FirebaseVision.getInstance().getVisionFaceDetector(
-        FirebaseVisionFaceDetectorOptions.Builder()
-            .setModeType(FirebaseVisionFaceDetectorOptions.FAST_MODE)
-            .setLandmarkType(FirebaseVisionFaceDetectorOptions.NO_LANDMARKS)
-            .setClassificationType(FirebaseVisionFaceDetectorOptions.NO_CLASSIFICATIONS)
-            .setMinFaceSize(0.15f)
-            .setTrackingEnabled(true)
-            .build()
+            FirebaseVisionFaceDetectorOptions.Builder()
+                    .setModeType(FirebaseVisionFaceDetectorOptions.FAST_MODE)
+                    .setLandmarkType(FirebaseVisionFaceDetectorOptions.NO_LANDMARKS)
+                    .setClassificationType(FirebaseVisionFaceDetectorOptions.NO_CLASSIFICATIONS)
+                    .setMinFaceSize(0.15f)
+                    .setTrackingEnabled(true)
+                    .build()
     )
 
     fun setImageToBlur(bitmap: Bitmap) {
@@ -52,7 +58,7 @@ class BlurViewGroup(context: Context, attrsSet: AttributeSet) : RelativeLayout(c
     private fun detectFaces() {
         srcBitmap?.let { bitmap ->
             detector.detectInImage(FirebaseVisionImage.fromBitmap(bitmap))
-                .addOnSuccessListener { processDetectedFaces(it) }
+                    .addOnSuccessListener { processDetectedFaces(it) }
         }
     }
 
@@ -60,9 +66,9 @@ class BlurViewGroup(context: Context, attrsSet: AttributeSet) : RelativeLayout(c
         faces.forEach { face ->
             with(face) {
                 rectangleList.add(com.nicolastelera.anonymize.FaceRectangle(
-                    boundingBox,
-                    headEulerAngleY,
-                    headEulerAngleZ
+                        boundingBox,
+                        headEulerAngleY,
+                        headEulerAngleZ
                 ))
             }
         }
@@ -76,10 +82,13 @@ class BlurViewGroup(context: Context, attrsSet: AttributeSet) : RelativeLayout(c
                     x = left.toFloat() * scaleFactor
                     y = top.toFloat() * scaleFactor
                     layoutParams = LinearLayout.LayoutParams(
-                        (width() * scaleFactor).toInt(),
-                        (height() * scaleFactor).toInt()
+                            (width() * scaleFactor).toInt(),
+                            (height() * scaleFactor).toInt()
                     )
-                    setOnClickListener { imageView.applyBlur(this) }
+                    setOnClickListener {
+                        rect.shouldBeBlurred = !rect.shouldBeBlurred
+                        imageView.updateBlurredZones()
+                    }
                 }
                 addView(this)
             }
@@ -98,29 +107,37 @@ class BlurViewGroup(context: Context, attrsSet: AttributeSet) : RelativeLayout(c
             srcBitmap?.let { setImageBitmap(it) }
         }
 
-        fun applyBlur(bounds: Rect) {
-            srcBitmap?.let { src ->
-                val output = Bitmap.createScaledBitmap(src, src.width, src.height, false)
-                val rs = RenderScript.create(context)
-                val script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
-                val inAlloc = Allocation.createFromBitmap(rs, src,
-                    Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_GRAPHICS_TEXTURE)
-                val outAlloc = Allocation.createFromBitmap(rs, output)
-                val launchOptions = with(bounds) {
-                    Script.LaunchOptions().apply {
-                        setX(left, right)
-                        setY(top, bottom)
-                    }
+        fun updateBlurredZones() {
+            srcBitmap?.let { srcBitmap ->
+                var src = srcBitmap.copy(Bitmap.Config.ARGB_8888, false)
+                rectangleList.forEach {
+                    if (!it.isApplied && it.shouldBeBlurred) src = applyBlur(src, it.bounds, 5)
                 }
-                script.apply {
-                    setRadius(25f)
-                    setInput(inAlloc)
-                    forEach(outAlloc, launchOptions)
-                }
-                outAlloc.copyTo(output)
-                rs.destroy()
-                setImageBitmap(output)
+                setImageBitmap(src)
             }
+        }
+
+        private fun applyBlur(src: Bitmap, bounds: Rect, iterations: Int): Bitmap {
+            val output = Bitmap.createScaledBitmap(src, src.width, src.height, false)
+            val rs = RenderScript.create(context)
+            val script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
+            val inAlloc = Allocation.createFromBitmap(rs, src,
+                    Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_GRAPHICS_TEXTURE)
+            val outAlloc = Allocation.createFromBitmap(rs, output)
+            val launchOptions = with(bounds) {
+                Script.LaunchOptions().apply {
+                    setX(left, right)
+                    setY(top, bottom)
+                }
+            }
+            script.apply {
+                setRadius(25f)
+                setInput(inAlloc)
+                forEach(outAlloc, launchOptions)
+            }
+            outAlloc.copyTo(output)
+            rs.destroy()
+            return if (iterations == 0) output else applyBlur(output, bounds, iterations - 1)
         }
     }
 }
