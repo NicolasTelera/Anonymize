8255ff7d5f595be8e92c234b77dc9263f6712973
Telera Nicolas <nicolas_telera@dm-c07vt0xqg1j1.groupevsc.com>
2018-09-13 13:45:35 +0200

    added import picture button (UI only)

--- app/src/main/java/com/nicolastelera/anonymize/MainActivity.kt
+++ app/src/main/java/com/nicolastelera/anonymize/MainActivity.kt
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -63,6 +64,10 @@ class MainActivity : AppCompatActivity() {
                     startActivityForResult(this, REQUEST_TAKE_PHOTO)
                 }
             }
+        }
+
+        importButton.setOnClickListener {
+            Toast.makeText(this, "importe picture", Toast.LENGTH_SHORT).show()
         }
 
         saveButton.setOnClickListener {
